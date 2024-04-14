45ce411f66758a5243353e4697e9aea800c46450
Mauser <klemenm02@gmail.com>
2024-04-14 19:17:05 +0200

    popravki

--- tests/test_prepare_environment.py
+++ tests/test_prepare_environment.py
@@ -3,7 +3,6 @@ import unittest
 class TestPrepareEnvironment(unittest.TestCase):
 
     def test_environment_setup(self):
-        # Tu dodajte preverjanje priprave testnega okolja
         pass
 
 if __name__ == '__main__':
